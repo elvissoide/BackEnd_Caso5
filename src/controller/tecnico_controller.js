b03dbda0d79b39ce31736d7f963a49a4a3e4adb4
Elvis Guanoluisa <elviserock@gmail.com>
2023-10-02 11:07:39 -0500

    Endpoints tecnicos finish

--- src/controller/tecnico_controller.js
+++ src/controller/tecnico_controller.js
@@ -10,39 +10,39 @@ const listarTecnicos = async (req, res) => {
     }
 };
 
-// const detalleCliente = async(req,res)=>{
-//     const {id} = req.params
-//     if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el cliente ${id}`});
-//     const cliente = await Cliente.findById(id).select("-createdAt -updatedAt -__v")
-//     res.status(200).json(cliente)
-// }
+const detalleTecnico = async(req,res)=>{
+    const {id} = req.params
+    if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el tecnico ${id}`});
+    const tecnico = await Tecnico.findById(id).select("-createdAt -updatedAt -__v")
+    res.status(200).json(tecnico)
+}
 
-// const actualizarCliente = async(req,res)=>{
-//     try {
-//         const {id} = req.params
-//         if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-//         if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el cliente ${id}`});
-//         await Cliente.findByIdAndUpdate(req.params.id,req.body)
-//         res.status(200).json({msg:"Actualización exitosa del cliente"})
-//     } 
-//     catch (error) {
-//         console.log(error)
-//     }
-// }
+const actualizarTecnico = async(req,res)=>{
+    try {
+        const {id} = req.params
+        if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+        if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el tecnico ${id}`});
+        await Tecnico.findByIdAndUpdate(req.params.id,req.body)
+        res.status(200).json({msg:"Actualización exitosa del tecnico"})
+    } 
+    catch (error) {
+        console.log(error)
+    }
+}
 
-// const eliminarCliente = async (req,res)=>{
-//     try{
-//         const {id} = req.params
-//         if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
-//         if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el cliente ${id}`})
-//         const {salida} = req.body
-//         await Cliente.findByIdAndRemove(id);
-//         res.status(200).json({msg:"Cliente eliminado exitosamente"})
-//     }
-//     catch(error){
-//         console.log(error)
-//     }
-// }
+const eliminarTecnico = async (req,res)=>{
+    try{
+        const {id} = req.params
+        if (Object.values(req.body).includes("")) return res.status(400).json({msg:"Lo sentimos, debes llenar todos los campos"})
+        if( !mongoose.Types.ObjectId.isValid(id) ) return res.status(404).json({msg:`Lo sentimos, no existe el tecnico ${id}`})
+        const {salida} = req.body
+        await Tecnico.findByIdAndRemove(id);
+        res.status(200).json({msg:"Tecnico eliminado exitosamente"})
+    }
+    catch(error){
+        console.log(error)
+    }
+}
 
 const registrarTecnico = async (req, res) => {
     try {
@@ -61,5 +61,8 @@ const registrarTecnico = async (req, res) => {
 
 export {
     registrarTecnico,
-    listarTecnicos
+    listarTecnicos,
+    actualizarTecnico,
+    detalleTecnico,
+    eliminarTecnico
 }
--- src/router/tecnico_routes.js
+++ src/router/tecnico_routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-import {listarTecnicos, registrarTecnico} from "../controller/tecnico_controller.js";
+import {actualizarTecnico, detalleTecnico, eliminarTecnico, listarTecnicos, registrarTecnico} from "../controller/tecnico_controller.js";
 import verificarAutenticacion from "../middlewares/autenticacion.js";
 
 const router = Router();
 
 router.get("/tecnicos",verificarAutenticacion,listarTecnicos);
 router.post("/tecnicos/registro", verificarAutenticacion, registrarTecnico);
-// router.get("/clientes/:id",verificarAutenticacion, detalleCliente);
-// router.put("/clientes/actualizar/:id", verificarAutenticacion,actualizarCliente);
-// router.delete("/clientes/eliminar/:id", verificarAutenticacion,eliminarCliente);
+router.get("/tecnicos/:id",verificarAutenticacion, detalleTecnico);
+router.put("/tecnicos/actualizar/:id", verificarAutenticacion,actualizarTecnico);
+router.delete("/tecnicos/eliminar/:id", verificarAutenticacion,eliminarTecnico);
 
 export default router;
